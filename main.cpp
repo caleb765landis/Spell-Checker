b79939dfa6cf5f1235fc65781b18db665bc75a0f
caleb765landis <caleb765landis@gmail.com>
2021-11-17 13:33:31 -0500

    Input dictionary file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+// main.cpp
+
+#include <fstream>
 #include <iostream>
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl;
-}
+  std::ifstream inFile;
+  std::string currentLine;
+
+  inFile.open("Dictionary.txt");
+
+  int lineNum = 1;
+  while (getline(inFile, currentLine)) {
+    cout << lineNum << " " << currentLine << endl;
+    lineNum++;
+  } // end while
+
+  inFile.close();
+} // end main
