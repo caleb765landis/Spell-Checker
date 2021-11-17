42487984001178a89f14c3436c553897140f4e41
caleb765landis <caleb765landis@gmail.com>
2021-11-17 15:07:21 -0500

    Finished hash function

--- main.cpp
+++ main.cpp
@@ -1,20 +1,39 @@
 // main.cpp
 
+#include <vector>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
+int hashFunc(string word, int charVal);
+
 int main() {
   std::ifstream inFile;
-  std::string currentLine;
+  std::string currentWord;
 
   inFile.open("Dictionary.txt");
 
   int lineNum = 1;
-  while (getline(inFile, currentLine)) {
-    cout << lineNum << " " << currentLine << endl;
+
+  while (getline(inFile, currentWord)) {
+    string currentWordCopy = currentWord;
+    int firstCharVal = tolower(currentWordCopy[0]) - 97;
+    int hashVal = hashFunc(currentWord, firstCharVal);
+
+    cout << lineNum << " " << currentWord << endl;
+    cout << firstCharVal << " " << hashVal << endl << endl;
     lineNum++;
   } // end while
 
   inFile.close();
 } // end main
+
+int hashFunc(string word, int charVal) {
+  int hashVal = charVal;
+  int wordLength = word.length();
+  for (int i = 0; i < wordLength; i++) {
+    // using Horner's rule
+    hashVal = abs(37 * hashVal + tolower(word[i]));
+  } // end for
+  return hashVal % 26;
+} // end hashFunc
